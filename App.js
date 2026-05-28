1e432f1bd5b524ae88647c7f1cab59f645cb5622
Dariusz Okoński <darek200180@gmail.com>
2026-05-28 07:21:21 +0200

    add game screens

--- App.js
+++ App.js
@@ -2,15 +2,16 @@ import { View, Text, StyleSheet } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.appContainer}>
+    <View style={styles.container}>
       <Text>Boilerplate</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  appContainer: {
+  container: {
     flex: 1,
+    backgroundColor: '#fff',
     justifyContent: 'center',
     alignItems: 'center',
   },
