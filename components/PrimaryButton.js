e3a6bfbd12b90459d450aa3f52be5a8497ea1f90
Dariusz Okoński <darek200180@gmail.com>
2026-05-29 08:18:36 +0200

    add android_ripple

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,11 +1,35 @@
-import { View, Text } from 'react-native';
+import { View, Text, Pressable, StyleSheet } from 'react-native';
 
 const PrimaryButton = ({ children }) => {
+  function pressHandler() {
+    console.log('!!! pressed');
+  }
+
   return (
-    <View>
-      <Text>{children}</Text>
-    </View>
+    <Pressable
+      onPress={pressHandler}
+      android_ripple={{ color: '#640233' }} //only android when press button
+    >
+      <View style={styles.buttonContainer}>
+        <Text style={styles.buttonText}>{children}</Text>
+      </View>
+    </Pressable>
   );
 };
 
 export default PrimaryButton;
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    backgroundColor: '#72063c',
+    borderRadius: 28,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    margin: 4,
+    elevation: 2, // shadow only for android
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+});
